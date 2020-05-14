19e421eb1d42ad2f22d9451f943201e0ab610286
Andrey Volodin <siddok@gmail.com>
2020-05-14 12:24:47 +0300

    Initial Commit

---
+++ UIKonfDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKonfDemo
+//
+//  Created by Andrey Volodin on 14.05.2020.
+//  Copyright © 2020 Andrey Volodin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
